fbb5a286679d2b5b5596c51171501f6fda49394d
fmbearmf <77757734+fmbearmf@users.noreply.github.com>
2024-12-29 22:38:20 -0800

    Zzzz

--- src/main.py
+++ src/main.py
@@ -5,12 +5,13 @@ import sqlite3
 import requests
 import os
 import subprocess
+import hashlib
 from datetime import datetime, timezone
 
-sub = "xertunposting"
+sub = os.environ["F_SUBREDDIT"]
 discordWebhookUrl = os.environ["F_WHOOK"]
 cotdWebhook = os.environ["F_SWHOOK"]
-cotdFlair = "cat of the day"
+cotdFlair = os.environ["F_FLAIR"]
 
 clientId = os.environ["F_ID"]
 clientSecret = os.environ["F_SECRET"]
@@ -30,15 +31,16 @@ def Loop(func):
     return Wrapper
 
 gitHashShort = subprocess.check_output(["git", "rev-parse", "--short", "HEAD"]).decode("ascii").strip()
+gitBranch    = subprocess.check_output(["git", "symbolic-ref", "HEAD"]).decode("ascii").strip()
 
 class SubredditFeed:
     def __init__(self, subreddit) -> None:
         self.subreddit = subreddit
         self.conn = sqlite3.connect('reddit_posts.db')
         self.CreateTable()
-        self.lastPostIds = self.GetLastPostIds()
-        self.botStartTime = datetime.now(timezone.utc)
         self.token = self.Auth()
+        self.botStartTime = datetime.now(timezone.utc)
+        self.cachedHashes = []
 
     def Auth(self):
         uri = "https://www.reddit.com/api/v1/access_token"
@@ -61,29 +63,61 @@ class SubredditFeed:
         
     def CreateTable(self):
         cursor = self.conn.cursor()
-        cursor.execute('''CREATE TABLE IF NOT EXISTS last_posts (
-                            subreddit TEXT,
-                            post_ids TEXT
+        cursor.execute('''CREATE TABLE IF NOT EXISTS post_hashes (
+                            hash TEXT PRIMARY KEY NOT NULL,
+                            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
                         )''')
         self.conn.commit()
-        
-    def GetLastPostIds(self):
+
+    def DBTime(self):
         cursor = self.conn.cursor()
-        cursor.execute("SELECT post_ids FROM last_posts WHERE subreddit=?", (self.subreddit,))
-        result = cursor.fetchone()
+        cursor.execute("SELECT MIN(created_at) FROM post_hashes")
+        result = cursor.fetchone()[0]
         if result:
-            return result[0].split(',')
+            time = datetime.fromisoformat(result).replace(tzinfo=timezone.utc)
+            return time
         else:
-            return []
+            return self.botStartTime
         
-    def UpdateLastPostIds(self, postId):
-        self.lastPostIds.append(postId)
-        if len(self.lastPostIds) > 10:
-            self.lastPostIds.pop(0)
-        
+    def Validate(self, permalink: str) -> bool:
+        hash = hashlib.sha256(permalink.encode()).hexdigest()
+        if hash in self.cachedHashes: return False
+
         cursor = self.conn.cursor()
-        cursor.execute("REPLACE INTO last_posts (subreddit, post_ids) VALUES (?, ?)", (self.subreddit, ','.join(self.lastPostIds)))
+        cursor.execute("SELECT 1 FROM post_hashes WHERE hash=?", (hash,))
+        result = cursor.fetchone()
+
+        if result: return False
+
+        cursor.execute("INSERT INTO post_hashes (hash) VALUES (?)", (hash,))
         self.conn.commit()
+
+        self.cachedHashes.append(hash)
+        if len(self.cachedHashes) > 4:
+            self.cachedHashes.pop(0)
+
+        return True
+
+    def FetchAvatar(self, user: str) -> str:
+        #url = f"https://api.reddit.com/user/{user}/about.json"
+        #headers = {"User-Agent": agent, "Authorization": f"bearer {self.token}"}
+        #res = requests.get(url, headers=headers)
+        #if res.status_code == 401:
+        #    self.Refresh()
+        #    headers['Authorization'] = f"bearer {self.token}"
+        #    res = requests.get(feedUrl, headers=headers)
+#
+        #if res.status_code == 200:
+        #    uri = res.json()['data']['icon_img']
+        #    parsed_uri = urlsplit(uri)
+        #    return urlunsplit((parsed_uri.scheme, parsed_uri.netloc, parsed_uri.path, '', ''))
+        #else:
+        #    print(f"EPIC FAIL! code: {res.status_code}")
+        #    print(res.json())
+        #    return None
+
+        return "https://www.redditstatic.com/avatars/defaults/v2/avatar_default_7.png" # avatar issues....
+
         
     def FetchFeed(self):
         feedUrl = f"https://oauth.reddit.com/r/{self.subreddit}.json"
@@ -103,7 +137,7 @@ class SubredditFeed:
     
     def PostToDiscord(self, post, webhook):
         author_name = post['data']['author']
-        author_avatar_url = f"https://avatar-resolver.vercel.app/reddit/{author_name}"
+        author_avatar_url = self.FetchAvatar(author_name)
         author_url = f"https://www.reddit.com/user/{author_name}"
 
         video_url = None
@@ -113,7 +147,7 @@ class SubredditFeed:
         image_url = post['data'].get('thumbnail')
         
         payload = {
-            "username": f"food eater 20 ({gitHashShort})",
+            "username": f"food eater 20",
             "avatar_url": "https://clipground.com/images/bread-loaf-png-3.png",
             "embeds": [
                 {
@@ -121,7 +155,7 @@ class SubredditFeed:
                     "url": f"https://www.reddit.com{post['data']['permalink']}",
                     "color": 16729344,
                     "author": {
-                        "name": f"u/{author_name}",
+                        "name": f"u/{author_name} on r/{self.subreddit}",
                         "url": author_url,
                         "icon_url": author_avatar_url
                     },
@@ -130,7 +164,7 @@ class SubredditFeed:
                     } if image_url.startswith('http') else {},
                     "description": "Unfortunately Reddit videos don't store audio (idk why). The video link sent has no audio." if (video_url != None) else "",
                     "footer": {
-                        "text": f"Version {gitHashShort} • r/{self.subreddit} • Posted at {datetime.fromtimestamp(post['data']['created_utc'], tz=timezone.utc)}"
+                        "text": f"• r/{self.subreddit}\n• Posted at {datetime.fromtimestamp(post['data']['created_utc'], tz=timezone.utc)}\n• Version: Commit #{gitHashShort} on {gitBranch}",
                     }
                 }
             ]
@@ -150,12 +184,17 @@ class SubredditFeed:
     def CheckPosts(self):
         feed = self.FetchFeed()
         if feed and 'data' in feed and 'children' in feed['data']:
+            time = self.DBTime()
+
             for post in feed['data']['children']:
-                postId = post['data']['id']
                 postTime = datetime.fromtimestamp(post['data']['created_utc'], tz=timezone.utc)
+                if (postTime <= time): 
+                    continue
+
+                permalink = post['data']['permalink']
                 flair = post['data'].get('link_flair_text')
 
-                if postId not in self.lastPostIds and postTime > self.botStartTime:
+                if self.Validate(permalink):
                     if flair == cotdFlair:
                         print("COTD detected:")
                         print("Title:", post['data']['title'])
@@ -170,7 +209,6 @@ class SubredditFeed:
                         print("Published:", postTime)
                         print("Flair: ", flair)
                         self.PostToDiscord(post, discordWebhookUrl)
-                    self.UpdateLastPostIds(postId)
         else:
             print("No posts in feed (or failed lol).")
             
